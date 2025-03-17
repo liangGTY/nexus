61d8d98a57a90d4ff0ba470c9590aeb843bbbc83
liangGTY <liangGTY@gmail.com>
2025-03-17 21:24:06 +0800

    first commit

---
+++ nexus-core/target/debug/deps/nexus_core-db6812f31ea2dace.d
@@ -0,0 +1,5 @@
+/Users/lianggty/src/rust/nexus/nexus-core/target/debug/deps/libnexus_core-db6812f31ea2dace.rmeta: src/lib.rs
+
+/Users/lianggty/src/rust/nexus/nexus-core/target/debug/deps/nexus_core-db6812f31ea2dace.d: src/lib.rs
+
+src/lib.rs:
